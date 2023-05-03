1621573b9ada31e478715e0423c57d78e36ab91d
rayush777 <115030307+rayush777@users.noreply.github.com>
2023-05-03 06:59:41 +0000

    server-test

--- index.js
+++ index.js
@@ -3,7 +3,11 @@ const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.status(200).json({ msg: "Hello World!" })
+})
+
+app.get('/test/', (req, res) => {
+  res.status(200).json({ msg: "Test called!!!" })
 })
 
 app.listen(port, () => {
